d374a4000d6f28c811c794ac94589273bcb4815a
Elena Herghelegiu <68195930+ElenaAlina@users.noreply.github.com>
2023-07-18 22:14:57 +0300

    Add files via upload
     //Write a Java program to print 'Hello' on screen and your name on a separate line.
        // Expected output (on separate line)
        // Hello,
        // Michael Dane!

---
+++ Exercise_1.java
@@ -0,0 +1,11 @@
+public class Exercise_1 {
+    //Write a Java program to print 'Hello' on screen and your name on a separate line.
+    // Expected output (on separate line)
+    // Hello,
+    // Michael Dane!
+    public static void main(String[] args) {
+        String hello = "Hello,";
+        String name = "Michael Dane";
+        System.out.println(hello + '\n' + name + "!");
+    }
+}
